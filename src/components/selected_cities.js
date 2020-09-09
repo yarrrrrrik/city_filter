47b055562b6edc0a9b1fe8a231a19443d05900e3
yarrrrrrik <60892661+yarrrrrrik@users.noreply.github.com>
2020-09-10 03:44:55 +0500

    Selected cities added

--- src/components/selected_cities.js
+++ src/components/selected_cities.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import {useSelector,useDispatch} from 'react-redux'
+import {removeSelectedCityAction} from '../redux/selected-cities/selected_cities_actions'
+
 
 function SelectedCities() {
+  const selectSelectedCities = useSelector(state => (state.selectedCities.selectedCitiesArray))
+  const dispatch = useDispatch()
+
   return (
-    <div className="spinner-border" >
-    wassup
+    <div className="selected-cities" >
+      <ul>
+        {
+          selectSelectedCities.map((city, i) => {
+            return <li key={i}> {city} <button onClick={ () => dispatch(removeSelectedCityAction(i)) }>x</button> </li>
+          })
+
+        }
+      </ul>
+
     </div>
   )
 }
--- src/redux/selected-cities/selected_cities_reducer.js
+++ src/redux/selected-cities/selected_cities_reducer.js
@@ -1,13 +1,16 @@
-import {SELECT_CITY} from './selected_cities_types'
+import {SELECT_CITY, REMOVE_CITY} from './selected_cities_types'
 
 const initial_state = {
-  selectedCities:[]
+  selectedCitiesArray:[]
 }
 
 export const selectedCitiesReducer = (state = initial_state, action) => {
   switch (action.type) {
     case SELECT_CITY:
-      return {...state,selectedCities:state.selectedCities.concat(action.selectedCity)}
+      return {...state, selectedCitiesArray:state.selectedCitiesArray.concat(action.selectedCity)}
+    case REMOVE_CITY:
+      state.selectedCitiesArray.splice(action.key, 1)
+      return {...state, selectedCitiesArray:state.selectedCitiesArray}
     default:
       return {...state}
   }
